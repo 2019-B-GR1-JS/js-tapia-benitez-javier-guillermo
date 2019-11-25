be23f20c4c78b3e5a5923520cfcc73351dda0a09
Javier Tapia <javier.tapia@epn.edu.ec>
2019-11-25 18:09:06 -0500

    Event and Property Binding

--- 03-Angular/components/src/app/app.component.ts
+++ 03-Angular/components/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'components';
+  arregloObjetos = [
+    {
+      id: 1, // number
+      "nombre": "Javier", // string
+      apellido: 'Tapia', // string
+      sueldo: 12.12, // number
+      casado: false, // boolean
+      comida: "https://www.laespanolaaceites.com/wp-content/uploads/2019/06/pizza-con-chorizo-jamon-y-queso-1080x671.jpg", //
+      carro: null,
+      ojos: undefined,
+    },
+    {
+      id: 2, // number
+      "nombre": "Nicole", // string
+      apellido: 'Pirca', // string
+      sueldo: 312.35, // number
+      casado: false, // boolean
+      comida: "https://ryukoch.com/images/food-blog/kimbap-t.jpg", //
+      carro: null,
+      ojos: undefined,
+    },
+    {
+      id: 3, // number
+      "nombre": "Lilly", // string
+      apellido: 'Aviles', // string
+      sueldo: 290.78, // number
+      casado: true, // boolean
+      comida: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG7klP9MGPGSN3qdzNbyea_5eSvcqe7aySFQUw06orwJ0MLOXl-Q&s", //
+      carro: null,
+      ojos: undefined,
+    }
+  ]
+
+  imprimirSueldo(sueldo: string) {
+    console.log(sueldo);
+  }
 }
--- 03-Angular/components/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-Angular/components/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,19 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.css']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input()
+  public titulo: string;
+
+  @Input()
+  public imagen: string;
+
+  @Input()
+  public textoImagen: string;
+
+  @Input() // Atributo Propiedad
+  public textoBoton: string;
+
+  @Input() // Atributo Propiedad
+  public tamanoImagen: string;
+
+  @Output() // EVENTO
+  public cambioSueldo = new EventEmitter();
+
+  @Output() // EVENTO
+  public cambioTamaño = new EventEmitter();
+
+  constructor() {
+    console.log('Instanciando');
+    this.tamanoImagen = "200px";
+  } // Instancia la clase (Ayuda -> Angular)
 
   ngOnInit() {
-  }
+    console.log('Esta listo');
+    console.log(this.titulo);
+    console.log(this.textoImagen);
+    console.log(this.textoBoton);
+  } // La clase está lista
 
-  saludar(){
+  ngOnDestroy(): void {
+  } // La clase está destruida
+
+  saludar() {
     alert("HOLAAAAAAAAAA");
   }
 
+  aumentoSueldo() {
+    //this.textoBoton = this.textoBoton + 1;
+    this.textoBoton = (Number(this.textoBoton) + 1).toString();
+    this.cambioSueldo.emit(this.textoBoton);
+  }
+
 }
+
+// class (etiqueta html) (web-component)
+// Instanciar a la clase
+// Clase esta lista
+// Clase se destruye
+
