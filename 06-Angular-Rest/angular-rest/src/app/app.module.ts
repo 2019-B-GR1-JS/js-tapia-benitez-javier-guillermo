ada45c2e5e83fc2408b938863c9a9a70f43779be
Javier Tapia <javier.tapia@epn.edu.ec>
2020-01-13 18:06:16 -0500

    PrimeNG y Angular Material

--- 06-Angular-Rest/angular-rest/src/app/app.module.ts
+++ 06-Angular-Rest/angular-rest/src/app/app.module.ts
@@ -7,6 +7,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {UsuarioRestService} from './services/rest/usuario-rest.service';
 import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {InputTextModule} from 'primeng/inputtext';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -19,6 +23,10 @@ import {ButtonModule} from 'primeng/button';
     FormsModule, // -> Formularios
     HttpClientModule, // -> HTTP
     ButtonModule,
+    TableModule,
+    InputTextModule,
+    BrowserAnimationsModule,
+    MatInputModule,
   ],
   providers: [
     UsuarioRestService
