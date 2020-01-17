c1748ec6a1ca3bd78f927ebba6c3e483636c31f5
Javier Tapia <javier.tapia@epn.edu.ec>
2020-01-17 18:04:37 -0500

    Funcion Editar - Modal Editar

--- 06-Angular-Rest/angular-rest/src/app/services/rest/usuario-rest.service.ts
+++ 06-Angular-Rest/angular-rest/src/app/services/rest/usuario-rest.service.ts
@@ -2,16 +2,29 @@
 // -> Solo 1 instancia del SERVICIO
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioRestService {
   // Inyeccion de dependencias
+  url = environment.url + '/usuario';
+
   constructor(
     // public readonly httpClient: HttpClient, // Servicio -> http
     private readonly _httpClient: HttpClient, // Servicio -> http
   ) {
 
+  }
+
+  editar(id: number, datos): Observable<any> {
+    const urlEditar = this.url + '/' + id;
+    return this._httpClient
+      .put(
+        urlEditar,
+        datos
+      );
   }
 }
